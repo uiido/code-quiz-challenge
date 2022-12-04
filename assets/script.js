07382b4e8809ac9594ef7097cadacc2030d39899
uiido <uiido@yahoo.com>
2022-12-04 16:42:22 -0500

    Did basic html layout, did very basic css

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,31 @@
 var questions = [
     {
-        title: "You can write CSS and/or Javscript in HTML files:",
+        question: "You can write CSS and/or Javscript in HTML files:",
         choices: ["true", "false"],
         answer: "true"
     },
     {
-        title: "HTML semmantic code doesn't include:",
+        question: "HTML semmantic code doesn't include:",
         choices: ["<p>", "<aside>", "<span>", "<footer>"],
         answer: "<span>"
     },
     {
-        title: "The style 'color' in CSS refers to the color of:",
+        question: "The style 'color' in CSS refers to the color of:",
         choices: ["the border color", "the text color", "the background color", "the text decoration color"],
         answer: "the text color"
     },
     {
-        title: "Media Queries only work on widths below 1000px:",
+        question: "Media Queries only work on widths below 1000px:",
         choices: ["true", "false"],
         answer: "false"
     },
     {
-        title: "What should enclose conditions in an if / else statement",
+        question: "What should enclose conditions in an if / else statement",
         choices: ["[]", "()", "{}", "nothing"],
         answer: "{}"
     },
     {
-        title: "In JavaScript, a boolean can be a number:",
+        question: "In JavaScript, a boolean can be a number:",
         choices: ["true", "false"],
         answer: "false"
     },
