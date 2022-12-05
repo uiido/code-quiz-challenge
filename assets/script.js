f2629fae641550779f91b8e47b97731ff38c1812
uiido <uiido@yahoo.com>
2022-12-04 20:19:35 -0500

    Hit a road block, no questions display - commenting everything out and starting again

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,45 @@
-var quizEl = document.querySelector(".quiz");
+var questionEl = document.querySelector(".question");
 var cursor = 0;
 
 var questions = [
     {
         question: "You can write CSS and/or Javscript in HTML files:",
         choices: ["true", "false"],
-        answer: "true"
+        answer: "true",
     },
     {
         question: "HTML semmantic code doesn't include:",
         choices: ["<p>", "<aside>", "<span>", "<footer>"],
-        answer: "<span>"
+        answer: "<span>",
     },
     {
         question: "The style 'color' in CSS refers to the color of:",
         choices: ["the border color", "the text color", "the background color", "the text decoration color"],
-        answer: "the text color"
+        answer: "the text color",
     },
     {
         question: "Media Queries only work on widths below 1000px:",
         choices: ["true", "false"],
-        answer: "false"
+        answer: "false",
     },
     {
         question: "What should enclose conditions in an if / else statement",
         choices: ["[]", "()", "{}", "nothing"],
-        answer: "{}"
+        answer: "{}",
     },
     {
         question: "In JavaScript, a boolean can be a number:",
         choices: ["true", "false"],
-        answer: "false"
+        answer: "false",
     },
 ];
 
-var displayQuiz = function () {
-    quizEl.querySelector('h3').textContent = questions[cursor].text;
-    quizEl.querySelector('#choices').innherHTML = null;
+var correctAnswers = ["true", "<span>", "the text color", "false", "{}", "false"]
+
+var displayQuestion = function () {
+    questionEl.querySelector('h2').textContent = questions[cursor].text;
+
+    questionEl.querySelector('#choices').innherHTML = null;
     for (var buttonLabel of questions[cursor].possible)
         var buttonEl = document.createElement('button');
     buttonEl.textContent = buttonLabel;
@@ -44,27 +47,23 @@ var displayQuiz = function () {
     questionEl.querySelector('#choices').appendChild(buttonEl);
 };
 
-var score = 0;
-var questionsIndex = 0;
-
 var advance = function (event) {
     var element = event.target;
-    if (element.matches('question button')) {
-        var answer = element.dataset.choice === correctAnswer[cursor];
+    if (element.matches('.question button')) {
+        var answer = element.dataset.choice === correctAnswers[cursor];
         console.log(element.dataset.choice);
         console.log(correctAnswers[cursor]);
         console.log(answer);
         if (cursor < questions.length - 1) {
             cursor++;
-            quizEl.dataset.index = cursor;
         }
-        displayQuiz();
+        displayQuestion();
     }
 };
 
 document.addEventListener('click', advance);
 
-displayQuiz();
+displayQuestion();
 
 
 
