57ff43c2e3767f2c85fbb3636b9244b5a3179225
uiido <uiido@yahoo.com>
2022-12-04 21:10:21 -0500

    added questions back to js

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        question: "You can write CSS and/or Javscript in HTML files:",
+        choices: ["true", "false"],
+        answer: "true"
+    },
+    {
+        question: "HTML semmantic code doesn't include:",
+        choices: ["<p>", "<aside>", "<span>", "<footer>"],
+        answer: "<span>"
+    },
+    {
+        question: "The style 'color' in CSS refers to the color of:",
+        choices: ["the border color", "the text color", "the background color", "the text decoration color"],
+        answer: "the text color"
+    },
+    {
+        question: "Media Queries only work on widths below 1000px:",
+        choices: ["true", "false"],
+        answer: "false"
+    },
+    {
+        question: "What should enclose conditions in an if / else statement",
+        choices: ["[]", "()", "{}", "nothing"],
+        answer: "{}"
+    },
+    {
+        question: "In JavaScript, a boolean can be a number:",
+        choices: ["true", "false"],
+        answer: "false"
+    },
+];
