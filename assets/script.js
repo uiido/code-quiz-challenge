046e9f863335c12de8375e09e8b6bd10184bac4f
uiido <uiido@yahoo.com>
2022-12-05 04:15:45 -0500

    adjusted minor JS and datasets

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,11 @@
 // Query selectors
 var quizStart = document.querySelector('#start');
 var questions = document.querySelectorAll('.questions');
-var quizIndex = 0;
-
-// Questions, options, answers for quiz
-// var questions = [
-//     {
-//         question: "You can write CSS and/or Javscript in HTML files:",
-//         choices: ["true", "false"],
-//         answer: "true"
-//     },
-//     {
-//         question: "HTML semmantic code doesn't include:",
-//         choices: ["<p>", "<aside>", "<span>", "<footer>"],
-//         answer: "<span>"
-//     },
-//     {
-//         question: "The style 'color' in CSS refers to the color of:",
-//         choices: ["the border color", "the text color", "the background color", "the text decoration color"],
-//         answer: "the text color"
-//     },
-//     {
-//         question: "Media Queries only work on widths below 1000px:",
-//         choices: ["true", "false"],
-//         answer: "false"
-//     },
-//     {
-//         question: "What should enclose conditions in an if / else statement",
-//         choices: ["[]", "()", "{}", "nothing"],
-//         answer: "{}"
-//     },
-//     {
-//         question: "In JavaScript, a boolean can be a number:",
-//         choices: ["true", "false"],
-//         answer: "false"
-//     },
-// ];
-
-
-
-
-
-// var correctAnswers = ["true", "<span>", "the text color", "false", "{}", "false"];
 
 // Variables
+var quizIndex = 0;
+var correctAnswers = ["a", "c", "b", "b", "c", "b"];
+
 
 
 // Timer starts when start button is selected, count down is shown
@@ -69,6 +31,10 @@ var displayQuestion = function () {
 var advance = function (event) {
     var element = event.target;
     if (element.matches('question button')) {
+        var answer = element.dataset.choice === correctAnswers[quizIndex];
+        console.log(element.dataset.choice);
+        console.log(correctAnswers[quizIndex++]);
+        console.log(answer);
         if (quizIndex < questions.length - 1) {
             quizIndex++;
         }
