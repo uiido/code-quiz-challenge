79147a8f5105bdb2dca44a779483364f7d2625f8
uiido <uiido@yahoo.com>
2022-12-05 01:22:49 -0500

    got questions to display and to progress when clicked - no options yet

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,71 @@
 // Query selectors
-var startQuizEl = document.getElementById("#start");
-var timeLeftEl = document.getElementById("#count-down");
+var quizEl = document.querySelector('#quiz');
+var quizIndex = 0;
 
 // Questions, options, answers for quiz
+// var questions = [
+//     {
+//         question: "You can write CSS and/or Javscript in HTML files:",
+//         choices: ["true", "false"],
+//         answer: "true"
+//     },
+//     {
+//         question: "HTML semmantic code doesn't include:",
+//         choices: ["<p>", "<aside>", "<span>", "<footer>"],
+//         answer: "<span>"
+//     },
+//     {
+//         question: "The style 'color' in CSS refers to the color of:",
+//         choices: ["the border color", "the text color", "the background color", "the text decoration color"],
+//         answer: "the text color"
+//     },
+//     {
+//         question: "Media Queries only work on widths below 1000px:",
+//         choices: ["true", "false"],
+//         answer: "false"
+//     },
+//     {
+//         question: "What should enclose conditions in an if / else statement",
+//         choices: ["[]", "()", "{}", "nothing"],
+//         answer: "{}"
+//     },
+//     {
+//         question: "In JavaScript, a boolean can be a number:",
+//         choices: ["true", "false"],
+//         answer: "false"
+//     },
+// ];
+
 var questions = [
-    {
-        question: "You can write CSS and/or Javscript in HTML files:",
-        choices: ["true", "false"],
-        answer: "true"
-    },
-    {
-        question: "HTML semmantic code doesn't include:",
-        choices: ["<p>", "<aside>", "<span>", "<footer>"],
-        answer: "<span>"
-    },
-    {
-        question: "The style 'color' in CSS refers to the color of:",
-        choices: ["the border color", "the text color", "the background color", "the text decoration color"],
-        answer: "the text color"
-    },
-    {
-        question: "Media Queries only work on widths below 1000px:",
-        choices: ["true", "false"],
-        answer: "false"
-    },
-    {
-        question: "What should enclose conditions in an if / else statement",
-        choices: ["[]", "()", "{}", "nothing"],
-        answer: "{}"
-    },
-    {
-        question: "In JavaScript, a boolean can be a number:",
-        choices: ["true", "false"],
-        answer: "false"
-    },
-];
+    "how fast",
+    "when do",
+    "who are",
+    "do you"
+]
 
 // Variables
-var secondsLeft = 60;
 
 
 
 
+
+// Displays questions on quiz
+var displayQuestion = function () {
+    quizEl.textContent = questions[quizIndex];
+};
+
+
+// moves the quiz forward
+var advance = function () {
+    if (quizIndex < questions.length - 1) {
+        quizIndex++;
+    }
+    displayQuestion();
+};
+
+quizEl.addEventListener('click', advance);
+
+displayQuestion()
 
 
 
