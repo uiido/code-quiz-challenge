ce00382e916493891104a0c9685c9f6dc0e8ba29
uiido <uiido@yahoo.com>
2022-12-06 02:11:54 -0500

    Added comments to all known areas where I am missing acceptance criteria or having errors/problems with acceptance criteria

--- assets/script.js
+++ assets/script.js
@@ -25,7 +25,7 @@ function setCountdown() {
 
         if (timeRemaining === 0) {
             clearInterval(timeInterval);
-            timerEl.textContent = "Time's up!";
+            timerEl.textContent = "Game's Ended!";
         }
     }, 1000);
 }
@@ -33,8 +33,10 @@ function setCountdown() {
 setCountdown();
 
 // Score variables
+// Score never displays as anything other than 75, even though I want it to match time when the quiz ends; 
+// I don't know how to identify wrong answers to attach a penalty to them
 var scoreEl = document.querySelector('#total-score');
-var score = timeRemaining;
+var score = timeRemaining--;
 var penalty = 10;
 scoreEl.innerHTML = score;
 
@@ -68,24 +70,42 @@ var advance = function (event) {
     }
 };
 
+
+// Quiz never seems to jump to end screen that I established no matter what functions I used. I set it
+// up as a question, which I know must be wrong, in an attempt to not have it display at the bottom of every question.
+// How can I correct this issue?
+// How do I stop the timer at the end screen?
+// How do I use the stopped timer value for the score??
+
+
 document.addEventListener('click', advance);
 
 displayQuestion();
 
 // // local storage
+// I don't think these variables are grabbing anything - as they never display any user submitted data
 var initialsInput = document.getElementById("initials");
 
 var initials = {
-    initials: initialsInput.value
+    initials: initialsInput.value.trim()
 }
 
+
+// I don't think this works for initials. Score seems fine but user input never appears with initials
 localStorage.setItem("score", score);
-localStorage.setItem("initials", initials);
+localStorage.setItem("initials", JSON.stringify(initials));
 
+// I don't think this is working correclty? It doesn't seem to grab any user input
+function renderInitials() {
+    var highScoreInitials = JSON.parse(localstorage.getItem("initials"));
+    if (highScoreInitials !== null) {
+        document.querySelector(".user-score").textContent = highScoreInitials + " | " + score
+    }
+}
 
-
-// Skip to end screen when out of time
-// Keep score
-// Penalty
-// Local storage
-// Submit score
+// THINGS THAT AREN'T WORKING
+// Quiz doesn't skip ahead when timer runs out
+// Score always remains at 75, even when timer runs out
+// How do you track incorrect answers to add a penalty?
+// Local storage displays the user's initials as "initials", "undefined", or "[ojbect]" in the console log, not any user input
+// Local storage doesn't display on the highscores board
